61aaddcd3198b982b74bec39e549dd16975fd7f6
thejoebaldwin <contact@humboldttechgroup.com>
2012-09-10 17:49:12 -0500

    Initial Checkin of Assignment 1

---
+++ Exp/Exp/Form1.Designer.vb
@@ -0,0 +1,313 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtAnswer1 = New System.Windows.Forms.TextBox()
+        Me.txtAnswer4 = New System.Windows.Forms.TextBox()
+        Me.txtAnswer7 = New System.Windows.Forms.TextBox()
+        Me.txtAnswer2 = New System.Windows.Forms.TextBox()
+        Me.txtAnswer5 = New System.Windows.Forms.TextBox()
+        Me.txtAnswer8 = New System.Windows.Forms.TextBox()
+        Me.txtAnswer3 = New System.Windows.Forms.TextBox()
+        Me.txtAnswer6 = New System.Windows.Forms.TextBox()
+        Me.txtAnswer9 = New System.Windows.Forms.TextBox()
+        Me.txtBase = New System.Windows.Forms.TextBox()
+        Me.txtExponent = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblAnswer1 = New System.Windows.Forms.Label()
+        Me.lblAnswer4 = New System.Windows.Forms.Label()
+        Me.lblAnswer7 = New System.Windows.Forms.Label()
+        Me.lblAnswer2 = New System.Windows.Forms.Label()
+        Me.lblAnswer5 = New System.Windows.Forms.Label()
+        Me.lblAnswer8 = New System.Windows.Forms.Label()
+        Me.lblAnswer3 = New System.Windows.Forms.Label()
+        Me.lblAnswer6 = New System.Windows.Forms.Label()
+        Me.lblAnswer9 = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtAnswer1
+        '
+        Me.txtAnswer1.Location = New System.Drawing.Point(74, 121)
+        Me.txtAnswer1.Name = "txtAnswer1"
+        Me.txtAnswer1.Size = New System.Drawing.Size(100, 20)
+        Me.txtAnswer1.TabIndex = 0
+        '
+        'txtAnswer4
+        '
+        Me.txtAnswer4.Location = New System.Drawing.Point(74, 160)
+        Me.txtAnswer4.Name = "txtAnswer4"
+        Me.txtAnswer4.Size = New System.Drawing.Size(100, 20)
+        Me.txtAnswer4.TabIndex = 1
+        '
+        'txtAnswer7
+        '
+        Me.txtAnswer7.Location = New System.Drawing.Point(74, 204)
+        Me.txtAnswer7.Name = "txtAnswer7"
+        Me.txtAnswer7.Size = New System.Drawing.Size(100, 20)
+        Me.txtAnswer7.TabIndex = 2
+        '
+        'txtAnswer2
+        '
+        Me.txtAnswer2.Location = New System.Drawing.Point(249, 121)
+        Me.txtAnswer2.Name = "txtAnswer2"
+        Me.txtAnswer2.Size = New System.Drawing.Size(100, 20)
+        Me.txtAnswer2.TabIndex = 3
+        '
+        'txtAnswer5
+        '
+        Me.txtAnswer5.Location = New System.Drawing.Point(249, 159)
+        Me.txtAnswer5.Name = "txtAnswer5"
+        Me.txtAnswer5.Size = New System.Drawing.Size(100, 20)
+        Me.txtAnswer5.TabIndex = 4
+        '
+        'txtAnswer8
+        '
+        Me.txtAnswer8.Location = New System.Drawing.Point(249, 203)
+        Me.txtAnswer8.Name = "txtAnswer8"
+        Me.txtAnswer8.Size = New System.Drawing.Size(100, 20)
+        Me.txtAnswer8.TabIndex = 5
+        '
+        'txtAnswer3
+        '
+        Me.txtAnswer3.Location = New System.Drawing.Point(423, 120)
+        Me.txtAnswer3.Name = "txtAnswer3"
+        Me.txtAnswer3.Size = New System.Drawing.Size(100, 20)
+        Me.txtAnswer3.TabIndex = 6
+        '
+        'txtAnswer6
+        '
+        Me.txtAnswer6.Location = New System.Drawing.Point(423, 159)
+        Me.txtAnswer6.Name = "txtAnswer6"
+        Me.txtAnswer6.Size = New System.Drawing.Size(100, 20)
+        Me.txtAnswer6.TabIndex = 7
+        '
+        'txtAnswer9
+        '
+        Me.txtAnswer9.Location = New System.Drawing.Point(423, 203)
+        Me.txtAnswer9.Name = "txtAnswer9"
+        Me.txtAnswer9.Size = New System.Drawing.Size(100, 20)
+        Me.txtAnswer9.TabIndex = 8
+        '
+        'txtBase
+        '
+        Me.txtBase.Location = New System.Drawing.Point(268, 29)
+        Me.txtBase.Name = "txtBase"
+        Me.txtBase.Size = New System.Drawing.Size(100, 20)
+        Me.txtBase.TabIndex = 9
+        Me.txtBase.Text = "3"
+        Me.txtBase.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'txtExponent
+        '
+        Me.txtExponent.Location = New System.Drawing.Point(268, 68)
+        Me.txtExponent.Name = "txtExponent"
+        Me.txtExponent.Size = New System.Drawing.Size(100, 20)
+        Me.txtExponent.TabIndex = 10
+        Me.txtExponent.Text = "1"
+        Me.txtExponent.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(215, 29)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(31, 13)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "Base"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(218, 68)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(52, 13)
+        Me.Label2.TabIndex = 12
+        Me.Label2.Text = "Exponent"
+        '
+        'lblAnswer1
+        '
+        Me.lblAnswer1.AutoSize = True
+        Me.lblAnswer1.Location = New System.Drawing.Point(13, 127)
+        Me.lblAnswer1.Name = "lblAnswer1"
+        Me.lblAnswer1.Size = New System.Drawing.Size(39, 13)
+        Me.lblAnswer1.TabIndex = 13
+        Me.lblAnswer1.Text = "Label3"
+        '
+        'lblAnswer4
+        '
+        Me.lblAnswer4.AutoSize = True
+        Me.lblAnswer4.Location = New System.Drawing.Point(13, 159)
+        Me.lblAnswer4.Name = "lblAnswer4"
+        Me.lblAnswer4.Size = New System.Drawing.Size(39, 13)
+        Me.lblAnswer4.TabIndex = 14
+        Me.lblAnswer4.Text = "Label4"
+        '
+        'lblAnswer7
+        '
+        Me.lblAnswer7.AutoSize = True
+        Me.lblAnswer7.Location = New System.Drawing.Point(13, 203)
+        Me.lblAnswer7.Name = "lblAnswer7"
+        Me.lblAnswer7.Size = New System.Drawing.Size(39, 13)
+        Me.lblAnswer7.TabIndex = 15
+        Me.lblAnswer7.Text = "Label5"
+        '
+        'lblAnswer2
+        '
+        Me.lblAnswer2.AutoSize = True
+        Me.lblAnswer2.Location = New System.Drawing.Point(190, 127)
+        Me.lblAnswer2.Name = "lblAnswer2"
+        Me.lblAnswer2.Size = New System.Drawing.Size(39, 13)
+        Me.lblAnswer2.TabIndex = 16
+        Me.lblAnswer2.Text = "Label6"
+        '
+        'lblAnswer5
+        '
+        Me.lblAnswer5.AutoSize = True
+        Me.lblAnswer5.Location = New System.Drawing.Point(193, 160)
+        Me.lblAnswer5.Name = "lblAnswer5"
+        Me.lblAnswer5.Size = New System.Drawing.Size(39, 13)
+        Me.lblAnswer5.TabIndex = 17
+        Me.lblAnswer5.Text = "Label7"
+        '
+        'lblAnswer8
+        '
+        Me.lblAnswer8.AutoSize = True
+        Me.lblAnswer8.Location = New System.Drawing.Point(193, 202)
+        Me.lblAnswer8.Name = "lblAnswer8"
+        Me.lblAnswer8.Size = New System.Drawing.Size(39, 13)
+        Me.lblAnswer8.TabIndex = 18
+        Me.lblAnswer8.Text = "Label8"
+        '
+        'lblAnswer3
+        '
+        Me.lblAnswer3.AutoSize = True
+        Me.lblAnswer3.Location = New System.Drawing.Point(371, 126)
+        Me.lblAnswer3.Name = "lblAnswer3"
+        Me.lblAnswer3.Size = New System.Drawing.Size(39, 13)
+        Me.lblAnswer3.TabIndex = 19
+        Me.lblAnswer3.Text = "Label9"
+        '
+        'lblAnswer6
+        '
+        Me.lblAnswer6.AutoSize = True
+        Me.lblAnswer6.Location = New System.Drawing.Point(374, 160)
+        Me.lblAnswer6.Name = "lblAnswer6"
+        Me.lblAnswer6.Size = New System.Drawing.Size(45, 13)
+        Me.lblAnswer6.TabIndex = 20
+        Me.lblAnswer6.Text = "Label10"
+        '
+        'lblAnswer9
+        '
+        Me.lblAnswer9.AutoSize = True
+        Me.lblAnswer9.Location = New System.Drawing.Point(377, 201)
+        Me.lblAnswer9.Name = "lblAnswer9"
+        Me.lblAnswer9.Size = New System.Drawing.Size(45, 13)
+        Me.lblAnswer9.TabIndex = 21
+        Me.lblAnswer9.Text = "Label11"
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(373, 68)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalculate.TabIndex = 22
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        Me.btnCalculate.UseWaitCursor = True
+        '
+        'btnCancel
+        '
+        Me.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnCancel.Location = New System.Drawing.Point(465, 57)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(75, 23)
+        Me.btnCancel.TabIndex = 23
+        Me.btnCancel.Text = "Cancel"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AcceptButton = Me.btnCalculate
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnCancel
+        Me.ClientSize = New System.Drawing.Size(596, 262)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.lblAnswer9)
+        Me.Controls.Add(Me.lblAnswer6)
+        Me.Controls.Add(Me.lblAnswer3)
+        Me.Controls.Add(Me.lblAnswer8)
+        Me.Controls.Add(Me.lblAnswer5)
+        Me.Controls.Add(Me.lblAnswer2)
+        Me.Controls.Add(Me.lblAnswer7)
+        Me.Controls.Add(Me.lblAnswer4)
+        Me.Controls.Add(Me.lblAnswer1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtExponent)
+        Me.Controls.Add(Me.txtBase)
+        Me.Controls.Add(Me.txtAnswer9)
+        Me.Controls.Add(Me.txtAnswer6)
+        Me.Controls.Add(Me.txtAnswer3)
+        Me.Controls.Add(Me.txtAnswer8)
+        Me.Controls.Add(Me.txtAnswer5)
+        Me.Controls.Add(Me.txtAnswer2)
+        Me.Controls.Add(Me.txtAnswer7)
+        Me.Controls.Add(Me.txtAnswer4)
+        Me.Controls.Add(Me.txtAnswer1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtAnswer1 As System.Windows.Forms.TextBox
+    Friend WithEvents txtAnswer4 As System.Windows.Forms.TextBox
+    Friend WithEvents txtAnswer7 As System.Windows.Forms.TextBox
+    Friend WithEvents txtAnswer2 As System.Windows.Forms.TextBox
+    Friend WithEvents txtAnswer5 As System.Windows.Forms.TextBox
+    Friend WithEvents txtAnswer8 As System.Windows.Forms.TextBox
+    Friend WithEvents txtAnswer3 As System.Windows.Forms.TextBox
+    Friend WithEvents txtAnswer6 As System.Windows.Forms.TextBox
+    Friend WithEvents txtAnswer9 As System.Windows.Forms.TextBox
+    Friend WithEvents txtBase As System.Windows.Forms.TextBox
+    Friend WithEvents txtExponent As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents lblAnswer1 As System.Windows.Forms.Label
+    Friend WithEvents lblAnswer4 As System.Windows.Forms.Label
+    Friend WithEvents lblAnswer7 As System.Windows.Forms.Label
+    Friend WithEvents lblAnswer2 As System.Windows.Forms.Label
+    Friend WithEvents lblAnswer5 As System.Windows.Forms.Label
+    Friend WithEvents lblAnswer8 As System.Windows.Forms.Label
+    Friend WithEvents lblAnswer3 As System.Windows.Forms.Label
+    Friend WithEvents lblAnswer6 As System.Windows.Forms.Label
+    Friend WithEvents lblAnswer9 As System.Windows.Forms.Label
+    Friend WithEvents btnCalculate As System.Windows.Forms.Button
+    Friend WithEvents btnCancel As System.Windows.Forms.Button
+
+End Class
---
+++ Exp/Exp/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private Sub btnCancel_Click(sender As System.Object, e As System.EventArgs) Handles btnCancel.Click
+        Application.Exit()
+    End Sub
+
+    Private Function displayResults(i As Integer)
+        CType(Me.Controls.Find("lblAnswer" & i.ToString(), True)(0), Label).Text = Convert.ToInt16(txtBase.Text).ToString() & "^" & Convert.ToInt16(txtExponent.Text) + (i - 1)
+        CType(Me.Controls.Find("txtAnswer" & i.ToString(), True)(0), TextBox).Text = Convert.ToInt16(txtBase.Text) ^ (Convert.ToInt16(txtExponent.Text) + (i - 1))
+        Return i - 1
+    End Function
+
+    Private Sub btnCalculate_Click(sender As System.Object, e As System.EventArgs) Handles btnCalculate.Click
+        If Not IsNumeric(txtBase.Text) Or Not IsNumeric(txtExponent.Text) Then MessageBox.Show("Please check your input") Else displayResults(displayResults(displayResults(displayResults(displayResults(displayResults(displayResults(displayResults(displayResults(9)))))))))
+    End Sub
+End Class
