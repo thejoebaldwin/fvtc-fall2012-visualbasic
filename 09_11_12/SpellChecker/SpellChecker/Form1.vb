54838ca612bd42fd9e549e6f29a9da06027ffaca
Joe Baldwin <baldwinjoe@gmail.com>
2012-09-11 09:19:29 -0500

    Adding spellchecker demo

---
+++ 09_11_12/SpellChecker/SpellChecker/Form1.vb
@@ -0,0 +1,15 @@
+﻿Imports System.IO
+Imports System.Text
+
+Public Class Form1
+    Private theDictionary As String
+
+    Private Function getDictionaryString()
+        Dim fileReader As New System.IO.StreamReader("../../dictionary.txt")
+        Dim sbDictionary As StringBuilder = New StringBuilder()
+        Do While fileReader.Peek() <> -1
+            sbDictionary.Append(fileReader.ReadLine())
+        Loop
+        Return (sbDictionary.ToString())
+    End Function
+End Class
