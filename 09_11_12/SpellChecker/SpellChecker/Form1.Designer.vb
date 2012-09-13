fdc9151ee593cc4f9ae2d5a8e7bd8056c2a25c5f
Joe Baldwin <contact@humboldttechgroup.com>
2012-09-13 10:02:28 -0500

    Update to spellchecker

--- 09_11_12/SpellChecker/SpellChecker/Form1.Designer.vb
+++ 09_11_12/SpellChecker/SpellChecker/Form1.Designer.vb
@@ -22,17 +22,83 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.txtWord = New System.Windows.Forms.TextBox()
+        Me.btnSpellCheck = New System.Windows.Forms.Button()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
         Me.SuspendLayout()
+        '
+        'txtWord
+        '
+        Me.txtWord.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtWord.Location = New System.Drawing.Point(17, 32)
+        Me.txtWord.Name = "txtWord"
+        Me.txtWord.Size = New System.Drawing.Size(100, 29)
+        Me.txtWord.TabIndex = 0
+        '
+        'btnSpellCheck
+        '
+        Me.btnSpellCheck.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSpellCheck.Location = New System.Drawing.Point(123, 32)
+        Me.btnSpellCheck.Name = "btnSpellCheck"
+        Me.btnSpellCheck.Size = New System.Drawing.Size(134, 29)
+        Me.btnSpellCheck.TabIndex = 1
+        Me.btnSpellCheck.Text = "Spell Check"
+        Me.btnSpellCheck.UseVisualStyleBackColor = True
+        '
+        'lblResult
+        '
+        Me.lblResult.AutoSize = True
+        Me.lblResult.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblResult.Location = New System.Drawing.Point(49, 82)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(103, 24)
+        Me.lblResult.TabIndex = 2
+        Me.lblResult.Text = "<lblResult>"
+        '
+        'btnExit
+        '
+        Me.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(277, 135)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(10, 10)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(30, 146)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(199, 147)
+        Me.ListBox1.TabIndex = 4
         '
         'Form1
         '
+        Me.AcceptButton = Me.btnSpellCheck
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.CancelButton = Me.btnExit
+        Me.ClientSize = New System.Drawing.Size(288, 331)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.btnSpellCheck)
+        Me.Controls.Add(Me.txtWord)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+    Friend WithEvents txtWord As System.Windows.Forms.TextBox
+    Friend WithEvents btnSpellCheck As System.Windows.Forms.Button
+    Friend WithEvents lblResult As System.Windows.Forms.Label
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
 
 End Class
--- 09_11_12/SpellChecker/SpellChecker/Form1.vb
+++ 09_11_12/SpellChecker/SpellChecker/Form1.vb
@@ -9,7 +9,57 @@ Public Class Form1
         Dim sbDictionary As StringBuilder = New StringBuilder()
         Do While fileReader.Peek() <> -1
             sbDictionary.Append(fileReader.ReadLine())
+            sbDictionary.Append(",")
         Loop
-        Return (sbDictionary.ToString())
+        Return sbDictionary.ToString()
     End Function
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        lblResult.Text = String.Empty
+
+        theDictionary = getDictionaryString()
+
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As System.Object, e As System.EventArgs) Handles btnExit.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub btnSpellCheck_Click(sender As System.Object, e As System.EventArgs) Handles btnSpellCheck.Click
+
+        Dim searchWord As String = txtWord.Text.ToLower().Trim()
+
+        Dim splitString As String() = {","}
+
+        Dim arrWords As String() = theDictionary.Split(splitString, StringSplitOptions.RemoveEmptyEntries)
+
+        ListBox1.Items.Clear()
+
+
+        For i As Integer = 0 To arrWords.Length - 1
+
+            Dim dictionaryString As String = arrWords(i)
+
+        Next
+
+
+        For Each dictionaryString As String In arrWords
+            Dim shorterString As String = searchWord.Substring(1, searchWord.Length - 1)
+
+            If dictionaryString = searchWord Then
+                ListBox1.Items.Add(dictionaryString)
+            ElseIf dictionaryString.Contains(shorterString) And dictionaryString.Length <= searchWord.Length + 2 Then
+                ListBox1.Items.Add(dictionaryString)
+            End If
+        Next
+
+        'If (theDictionary.IndexOf(searchWord) > -1) Then
+        '    lblResult.Text = "Spelling Correct!"
+        'Else
+        '    lblResult.Text = "Not Spelled Correctly"
+        'End If
+
+
+    End Sub
 End Class
