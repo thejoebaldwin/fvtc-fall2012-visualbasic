b632eac6f92f073d702fa4b0084bb5674610611e
Joe Baldwin <baldwinjoe@gmail.com>
2012-09-17 17:13:43 -0500

    Spellchecker for PM class

---
+++ 09_17_12/SpellChecker/SpellChecker/Form1.vb
@@ -0,0 +1,21 @@
+﻿Imports System.IO
+Imports System.Text
+
+Public Class Form1
+    Private theDictionary As String
+
+    Private Function getDictionaryString()
+        Dim fileReader As New System.IO.StreamReader("../../dictionary.txt")
+        Dim sbDictionary As StringBuilder = New StringBuilder()
+        Do While fileReader.Peek() <> -1
+            sbDictionary.Append(fileReader.ReadLine())
+            sbDictionary.Append(",")
+        Loop
+        Return sbDictionary.ToString()
+    End Function
+
+ 
+
+
+  
+End Class
