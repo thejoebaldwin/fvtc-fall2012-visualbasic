456d99b4707f28930e5a625a51dffffc229787ca
thejoebaldwin <contact@humboldttechgroup.com>
2012-10-08 18:15:40 -0500

    Ice Cream Order for data structure demo

---
+++ 10_08_12/Collections/Collections/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+
+
+    Private arrScoops As ArrayList
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        lblScoops.Text = String.Empty
+        lblTotal.Text = String.Empty
+        arrScoops = New ArrayList()
+        displayScoops()
+    End Sub
+
+    Private Sub displayScoops()
+     
+    End Sub
+
+    Private Sub btnAdd_Click(sender As System.Object, e As System.EventArgs) Handles btnAdd.Click
+        arrScoops.Add(ListBox1.SelectedItem)
+        displayScoops()
+    End Sub
+
+    Private Sub btnRemove_Click(sender As System.Object, e As System.EventArgs) Handles btnRemove.Click
+        arrScoops.Remove(ListBox1.SelectedItem)
+        displayScoops()
+    End Sub
+End Class
---
+++ 10_08_12/Collections/Collections/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Collections")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("Collections")> 
+<Assembly: AssemblyCopyright("Copyright ©  2012")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("08197f53-572c-4762-bf30-4bdfe7d8f2aa")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
