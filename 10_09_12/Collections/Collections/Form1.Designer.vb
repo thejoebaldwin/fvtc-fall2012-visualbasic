a7640841ec9475561db04d858788d318118caaba
Joe Baldwin <baldwinjoe@gmail.com>
2012-10-09 09:19:57 -0500

    Add starter project for class

---
+++ 10_09_12/Collections/Collections/Form1.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.lblScoops = New System.Windows.Forms.Label()
+        Me.btnRemove = New System.Windows.Forms.Button()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 24
+        Me.ListBox1.Items.AddRange(New Object() {"Chocolate", "Vanilla", "Mint", "Carmel Fudge Swirl", "Neopolitan"})
+        Me.ListBox1.Location = New System.Drawing.Point(28, 12)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(182, 124)
+        Me.ListBox1.TabIndex = 0
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAdd.Location = New System.Drawing.Point(249, 28)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(132, 43)
+        Me.btnAdd.TabIndex = 1
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'lblScoops
+        '
+        Me.lblScoops.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblScoops.Location = New System.Drawing.Point(12, 217)
+        Me.lblScoops.Name = "lblScoops"
+        Me.lblScoops.Size = New System.Drawing.Size(419, 94)
+        Me.lblScoops.TabIndex = 2
+        Me.lblScoops.Text = "<lblScoops>"
+        '
+        'btnRemove
+        '
+        Me.btnRemove.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnRemove.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnRemove.Location = New System.Drawing.Point(249, 88)
+        Me.btnRemove.Name = "btnRemove"
+        Me.btnRemove.Size = New System.Drawing.Size(132, 43)
+        Me.btnRemove.TabIndex = 3
+        Me.btnRemove.Text = "Remove"
+        Me.btnRemove.UseVisualStyleBackColor = True
+        '
+        'lblTotal
+        '
+        Me.lblTotal.AutoSize = True
+        Me.lblTotal.Location = New System.Drawing.Point(16, 167)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(53, 13)
+        Me.lblTotal.TabIndex = 4
+        Me.lblTotal.Text = "<lblTotal>"
+        '
+        'Form1
+        '
+        Me.AcceptButton = Me.btnAdd
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.CancelButton = Me.btnRemove
+        Me.ClientSize = New System.Drawing.Size(443, 320)
+        Me.Controls.Add(Me.lblTotal)
+        Me.Controls.Add(Me.btnRemove)
+        Me.Controls.Add(Me.lblScoops)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "Form1"
+        Me.Text = "Ice Cream Order"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents btnAdd As System.Windows.Forms.Button
+    Friend WithEvents lblScoops As System.Windows.Forms.Label
+    Friend WithEvents btnRemove As System.Windows.Forms.Button
+    Friend WithEvents lblTotal As System.Windows.Forms.Label
+
+End Class
---
+++ 10_09_12/Collections/Collections/Form1.vb
@@ -0,0 +1,48 @@
+﻿Public Class Form1
+
+    ''' <summary>
+    ''' initialize form
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        lblScoops.Text = String.Empty
+        lblTotal.Text = String.Empty
+        
+        'display any initial values (empty order)
+        displayScoops()
+    End Sub
+
+    ''' <summary>
+    ''' code to display the scoops ordered
+    ''' </summary>
+    ''' <remarks></remarks>
+    Private Sub displayScoops()
+        
+    End Sub
+
+    ''' <summary>
+    ''' Add a scoop to the order
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub btnAdd_Click(sender As System.Object, e As System.EventArgs) Handles btnAdd.Click
+
+        'display updated scoops in order
+        displayScoops()
+    End Sub
+
+    ''' <summary>
+    ''' Remove a scoop from the order
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub btnRemove_Click(sender As System.Object, e As System.EventArgs) Handles btnRemove.Click
+        
+        'display updated scoops in order
+        displayScoops()
+    End Sub
+End Class
