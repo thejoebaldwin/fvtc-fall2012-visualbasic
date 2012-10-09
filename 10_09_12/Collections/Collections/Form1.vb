dfc6d2da0b7aa0328fab04eb370b3036e9b4eab0
thejoebaldwin <contact@humboldttechgroup.com>
2012-10-09 11:09:32 -0500

    code from class

--- 10_09_12/Collections/Collections/Form1.vb
+++ 10_09_12/Collections/Collections/Form1.vb
@@ -1,7 +1,10 @@
 ﻿Public Class Form1
 
+
+    Dim arrScoops As ArrayList
+
     ''' <summary>
-    ''' initialize form
+    ''' 
     ''' </summary>
     ''' <param name="sender"></param>
     ''' <param name="e"></param>
@@ -9,7 +12,10 @@
     Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
         lblScoops.Text = String.Empty
         lblTotal.Text = String.Empty
-        
+
+        arrScoops = New ArrayList()
+
+
         'display any initial values (empty order)
         displayScoops()
     End Sub
@@ -19,7 +25,12 @@
     ''' </summary>
     ''' <remarks></remarks>
     Private Sub displayScoops()
-        
+        Dim output As String = ""
+        For i As Integer = 0 To arrScoops.Count - 1
+            output = output & arrScoops(i) & ","
+        Next
+        output = output.TrimEnd(",")
+        lblScoops.Text = output
     End Sub
 
     ''' <summary>
@@ -29,6 +40,15 @@
     ''' <param name="e"></param>
     ''' <remarks></remarks>
     Private Sub btnAdd_Click(sender As System.Object, e As System.EventArgs) Handles btnAdd.Click
+        Dim flavor As String = ListBox1.SelectedItem
+
+        'For i As Integer = 0 To arrScoops.Count - 1
+        '    If arrScoops(i) = String.Empty Then
+        '        arrScoops(i) = flavor
+        '        Exit For
+        '    End If
+        'Next
+        arrScoops.Add(flavor)
 
         'display updated scoops in order
         displayScoops()
@@ -41,7 +61,16 @@
     ''' <param name="e"></param>
     ''' <remarks></remarks>
     Private Sub btnRemove_Click(sender As System.Object, e As System.EventArgs) Handles btnRemove.Click
-        
+        Dim flavor As String = ListBox1.SelectedItem
+
+        If arrScoops.Contains(flavor) Then
+            arrScoops.Remove(flavor)
+        Else
+            MessageBox.Show("dont have that flavor ordered!")
+
+        End If
+
+
         'display updated scoops in order
         displayScoops()
     End Sub
