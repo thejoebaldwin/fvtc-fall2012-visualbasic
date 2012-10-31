8ddf5ab0e5f51ed5280e94f5202afd3abe152751
thejoebaldwin <baldwinjoe@gmail.com>
2012-10-30 23:50:23 -0500

    added completed candy project

---
+++ 10_30_12/CandyProject/Candy.vb
@@ -0,0 +1,48 @@
+﻿Public Class Candy
+    'name
+    'flavor
+    'quantity
+    Private _name As String
+    Private _flavor As String
+    Private _quantity As Integer
+
+
+    Public Sub New()
+        _quantity = 1
+    End Sub
+
+
+    Public Property Name As String
+        Get
+            Return _name
+        End Get
+        Set(ByVal value As String)
+            _name = value
+        End Set
+    End Property
+
+    Public Property Flavor As String
+        Get
+            Return _flavor
+        End Get
+        Set(ByVal value As String)
+            _flavor = value
+        End Set
+    End Property
+
+
+    Public ReadOnly Property Quantity As Integer
+        Get
+            Return _quantity
+        End Get
+    End Property
+
+    Public Sub AddOne()
+        _quantity += 1
+    End Sub
+
+    Public Sub EatOne()
+        _quantity -= 1
+    End Sub
+
+End Class
---
+++ 10_30_12/CandyProject/Form1.vb
@@ -0,0 +1,75 @@
+﻿Public Class Form1
+    Public arrCandy As Collection
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        arrCandy = New Collection()
+        lblQty.Text = String.Empty
+    End Sub
+
+    Private Sub btnAdd_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAdd.Click
+        Dim myCandy As Candy = New Candy()
+        myCandy.Name = txtName.Text
+        myCandy.Flavor = txtFlavor.Text
+
+        If arrCandy.Contains(myCandy.Name) Then
+            Dim tempCandy As Candy = arrCandy(myCandy.Name)
+            tempCandy.AddOne()
+        Else
+            arrCandy.Add(myCandy, myCandy.Name)
+        End If
+
+        updateListBox()
+    End Sub
+
+    Public Sub updateListBox()
+
+        lstCandy.Items.Clear()
+        For i As Integer = 1 To arrCandy.Count
+            Dim tempCandy As Candy = arrCandy(i)
+            lstCandy.Items.Add(tempCandy.Name)
+        Next
+
+    End Sub
+
+    Private Sub btnEat_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnEat.Click
+
+
+
+     
+
+        If arrCandy.Contains(lstCandy.SelectedItem) Then
+            Dim tempCandy As Candy = arrCandy(lstCandy.SelectedItem)
+            tempCandy.EatOne()
+            If (tempCandy.Quantity = 0) Then
+                arrCandy.Remove(lstCandy.SelectedItem)
+            End If
+        End If
+     
+        
+
+
+
+
+        updateListBox()
+
+
+    End Sub
+
+    Private Sub lstCandy_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lstCandy.SelectedIndexChanged
+        Dim candyName As String = lstCandy.SelectedItem
+        Dim isFound As Boolean = False
+        For i As Integer = 1 To arrCandy.Count
+            If candyName = arrCandy(i).Name Then
+                Dim tempCandy As Candy = arrCandy(i)
+                lblQty.Text = tempCandy.Quantity
+                txtName.Text = tempCandy.Name
+                txtFlavor.Text = tempCandy.Flavor
+                Exit For
+            End If
+        Next
+
+
+
+
+    End Sub
+End Class
