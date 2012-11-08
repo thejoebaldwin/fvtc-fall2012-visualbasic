3c1ac0fb0152941aac5cafe8d63283b9bd036ebf
thejoebaldwin <contact@humboldttechgroup.com>
2012-11-08 09:34:55 -0600

    update for base 11/8

---
+++ 11_08_12/Registrar/frmEdit.vb
@@ -0,0 +1,6 @@
+﻿Public Class frmEdit
+
+    Private Sub btnCancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCancel.Click
+        Me.Close()
+    End Sub
+End Class
