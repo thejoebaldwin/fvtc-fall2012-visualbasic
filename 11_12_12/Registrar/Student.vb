35379a4f40d5c7673d43f378fc337a77e7be1081
Joe Baldwin <contact@humboldttechgroup.com>
2012-11-12 17:49:34 -0600

    code FOR class

---
+++ 11_12_12/Registrar/Student.vb
@@ -0,0 +1,46 @@
+﻿Public Class Student
+    Private _firstName As String
+    Private _lastName As String
+    Private _studentID As String
+
+
+    Public Sub New()
+
+    End Sub
+
+    Public Sub New(ByVal sFirstName As String, ByVal sLastName As String, ByVal sStudentID As String)
+        _firstName = sFirstName
+        _lastName = sLastName
+        _studentID = sStudentID
+    End Sub
+
+
+    Public Property FirstName As String
+        Get
+            Return _firstName
+        End Get
+        Set(ByVal value As String)
+            _firstName = value
+        End Set
+    End Property
+
+
+
+    Public Property LastName As String
+        Get
+            Return _lastName
+        End Get
+        Set(ByVal value As String)
+            _lastName = value
+        End Set
+    End Property
+
+    Public Property StudentID As String
+        Get
+            Return _studentID
+        End Get
+        Set(ByVal value As String)
+            _studentID = value
+        End Set
+    End Property
+End Class
