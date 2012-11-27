eb06b7a6d9bce68926c3f5346cb48c4694ad3f5b
Joe Baldwin <contact@humboldttechgroup.com>
2012-11-27 11:34:35 -0600

    added mysql client

---
+++ 11_27_12/databaseMySql/databaseMySql/Form1.Designer.vb
@@ -0,0 +1,127 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lbData = New System.Windows.Forms.ListBox()
+        Me.txtUserName = New System.Windows.Forms.TextBox()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnQuery = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lbData
+        '
+        Me.lbData.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbData.FormattingEnabled = True
+        Me.lbData.ItemHeight = 24
+        Me.lbData.Location = New System.Drawing.Point(36, 165)
+        Me.lbData.Name = "lbData"
+        Me.lbData.Size = New System.Drawing.Size(366, 172)
+        Me.lbData.TabIndex = 0
+        '
+        'txtUserName
+        '
+        Me.txtUserName.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtUserName.Location = New System.Drawing.Point(116, 2)
+        Me.txtUserName.Name = "txtUserName"
+        Me.txtUserName.Size = New System.Drawing.Size(276, 29)
+        Me.txtUserName.TabIndex = 1
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPassword.Location = New System.Drawing.Point(116, 40)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(276, 29)
+        Me.txtPassword.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(97, 24)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Username"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(14, 40)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(92, 24)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Password"
+        '
+        'btnQuery
+        '
+        Me.btnQuery.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnQuery.Location = New System.Drawing.Point(67, 106)
+        Me.btnQuery.Name = "btnQuery"
+        Me.btnQuery.Size = New System.Drawing.Size(300, 40)
+        Me.btnQuery.TabIndex = 5
+        Me.btnQuery.Text = "Query"
+        Me.btnQuery.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(31, 79)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(407, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Check blackboard for this - don't put passwords in github for public facing datab" & _
+    "ases!"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(450, 358)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.btnQuery)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.txtUserName)
+        Me.Controls.Add(Me.lbData)
+        Me.Name = "Form1"
+        Me.Text = "MySQL Client"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lbData As System.Windows.Forms.ListBox
+    Friend WithEvents txtUserName As System.Windows.Forms.TextBox
+    Friend WithEvents txtPassword As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents btnQuery As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+
+End Class
---
+++ 11_27_12/databaseMySql/databaseMySql/Form1.vb
@@ -0,0 +1,48 @@
+﻿
+Imports System.Data
+Imports MySql.Data
+Imports MySql.Data.MySqlClient
+
+
+Public Class Form1
+
+    Private Const CONNECTION_STRING = "server=23.23.147.156; user id={0}; password={0}; database=vb_demo; pooling=false;"
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+    
+    End Sub
+
+    Private Sub btnQuery_Click(sender As System.Object, e As System.EventArgs) Handles btnQuery.Click
+
+        'declare these variables outside the try catch so we can close the connection in the finally clause
+        Dim dbConnection As MySqlConnection = Nothing
+        Dim dbAdapter As MySqlDataAdapter
+        Dim ds As DataSet = New DataSet()
+        Dim query As String
+        Try
+            'get username and password from the form
+            Dim connectionWithCredentials As String = String.Format(CONNECTION_STRING, txtUserName.Text, txtPassword.Text)
+            'set up connection
+            dbConnection = New MySqlConnection(CONNECTION_STRING)
+            query = "SELECT * FROM helloworld;"
+            dbConnection.Open()
+            dbAdapter = New MySqlDataAdapter(query, dbConnection)
+            'get the data
+            dbAdapter.Fill(ds)
+            'output
+            For Each dr As DataRow In ds.Tables(0).Rows
+                lbData.Items.Add(dr("message"))
+            Next
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            'if not null then close the conenction
+            If (Not dbConnection Is Nothing) Then
+                dbConnection.Close()
+            End If
+
+        End Try
+
+
+    End Sub
+End Class
