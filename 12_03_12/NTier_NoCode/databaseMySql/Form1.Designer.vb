6224bba5de272ecc299ebcce0b9e4541c60a5139
Joe Baldwin <contact@humboldttechgroup.com>
2012-12-03 19:32:10 -0600

    added ntier no code

---
+++ 12_03_12/NTier_NoCode/databaseMySql/Form1.Designer.vb
@@ -0,0 +1,232 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lbData = New System.Windows.Forms.ListBox()
+        Me.txtUserName = New System.Windows.Forms.TextBox()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnQuery = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtMessage = New System.Windows.Forms.TextBox()
+        Me.btnInsert = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.lblID = New System.Windows.Forms.Label()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'lbData
+        '
+        Me.lbData.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbData.FormattingEnabled = True
+        Me.lbData.ItemHeight = 24
+        Me.lbData.Location = New System.Drawing.Point(53, 174)
+        Me.lbData.Name = "lbData"
+        Me.lbData.Size = New System.Drawing.Size(366, 148)
+        Me.lbData.TabIndex = 0
+        '
+        'txtUserName
+        '
+        Me.txtUserName.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtUserName.Location = New System.Drawing.Point(120, 24)
+        Me.txtUserName.Name = "txtUserName"
+        Me.txtUserName.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtUserName.Size = New System.Drawing.Size(276, 29)
+        Me.txtUserName.TabIndex = 1
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPassword.Location = New System.Drawing.Point(120, 62)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtPassword.Size = New System.Drawing.Size(276, 29)
+        Me.txtPassword.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(16, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(97, 24)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Username"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(18, 62)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(92, 24)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Password"
+        '
+        'btnQuery
+        '
+        Me.btnQuery.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnQuery.Location = New System.Drawing.Point(71, 128)
+        Me.btnQuery.Name = "btnQuery"
+        Me.btnQuery.Size = New System.Drawing.Size(300, 40)
+        Me.btnQuery.TabIndex = 5
+        Me.btnQuery.Text = "Query"
+        Me.btnQuery.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(35, 101)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(407, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Check blackboard for this - don't put passwords in github for public facing datab" & _
+            "ases!"
+        '
+        'txtMessage
+        '
+        Me.txtMessage.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtMessage.Location = New System.Drawing.Point(24, 83)
+        Me.txtMessage.Multiline = True
+        Me.txtMessage.Name = "txtMessage"
+        Me.txtMessage.Size = New System.Drawing.Size(233, 169)
+        Me.txtMessage.TabIndex = 7
+        '
+        'btnInsert
+        '
+        Me.btnInsert.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnInsert.Location = New System.Drawing.Point(27, 261)
+        Me.btnInsert.Name = "btnInsert"
+        Me.btnInsert.Size = New System.Drawing.Size(243, 40)
+        Me.btnInsert.TabIndex = 10
+        Me.btnInsert.Text = "Insert"
+        Me.btnInsert.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btnDelete)
+        Me.GroupBox1.Controls.Add(Me.btnUpdate)
+        Me.GroupBox1.Controls.Add(Me.lblID)
+        Me.GroupBox1.Controls.Add(Me.txtName)
+        Me.GroupBox1.Controls.Add(Me.txtMessage)
+        Me.GroupBox1.Controls.Add(Me.btnInsert)
+        Me.GroupBox1.Location = New System.Drawing.Point(541, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(321, 398)
+        Me.GroupBox1.TabIndex = 11
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Message"
+        '
+        'txtName
+        '
+        Me.txtName.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtName.Location = New System.Drawing.Point(27, 45)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(230, 29)
+        Me.txtName.TabIndex = 7
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Controls.Add(Me.lbData)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.txtUserName)
+        Me.GroupBox2.Controls.Add(Me.btnQuery)
+        Me.GroupBox2.Controls.Add(Me.txtPassword)
+        Me.GroupBox2.Controls.Add(Me.Label1)
+        Me.GroupBox2.Location = New System.Drawing.Point(35, 12)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(460, 343)
+        Me.GroupBox2.TabIndex = 12
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Connection"
+        '
+        'lblID
+        '
+        Me.lblID.AutoSize = True
+        Me.lblID.Location = New System.Drawing.Point(27, 20)
+        Me.lblID.Name = "lblID"
+        Me.lblID.Size = New System.Drawing.Size(40, 13)
+        Me.lblID.TabIndex = 11
+        Me.lblID.Text = "<lblID>"
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnUpdate.Location = New System.Drawing.Point(30, 303)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(243, 40)
+        Me.btnUpdate.TabIndex = 12
+        Me.btnUpdate.Text = "Update"
+        Me.btnUpdate.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDelete.Location = New System.Drawing.Point(30, 349)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(243, 40)
+        Me.btnDelete.TabIndex = 13
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(887, 439)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "MySQL Client"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents lbData As System.Windows.Forms.ListBox
+    Friend WithEvents txtUserName As System.Windows.Forms.TextBox
+    Friend WithEvents txtPassword As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents btnQuery As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtMessage As System.Windows.Forms.TextBox
+    Friend WithEvents btnInsert As System.Windows.Forms.Button
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents txtName As System.Windows.Forms.TextBox
+    Friend WithEvents lblID As System.Windows.Forms.Label
+    Friend WithEvents btnDelete As System.Windows.Forms.Button
+    Friend WithEvents btnUpdate As System.Windows.Forms.Button
+
+End Class
---
+++ 12_03_12/NTier_NoCode/databaseMySql/Form1.vb
@@ -0,0 +1,139 @@
+﻿
+Imports MySql.Data
+Imports MySql.Data.MySqlClient
+Imports System.Data
+
+
+Public Class Form1
+
+    Private Const CONNECTION_STRING = "server=23.23.147.156; user id={0}; password={1}; database=vb_demo; pooling=false;"
+
+  
+    Private collMessage As Collection = New Collection()
+    Private collName As Collection = New Collection()
+    Private collID As Collection = New Collection()
+
+
+    Private Sub RefreshData()
+        Dim connectionWithCredentials As String = String.Format(CONNECTION_STRING, txtUserName.Text, txtPassword.Text)
+
+        Dim dbConnection As MySqlConnection = Nothing
+        Dim dbAdapter As MySqlDataAdapter
+        Dim ds As DataSet = New DataSet()
+        Dim query As String
+
+        Try
+
+            dbConnection = New MySqlConnection(connectionWithCredentials)
+            dbConnection.Open()
+            query = "SELECT * FROM helloworld"
+            dbAdapter = New MySqlDataAdapter(query, dbConnection)
+            dbAdapter.Fill(ds)
+            lbData.Items.Clear()
+            collMessage.Clear()
+            collName.Clear()
+            collID.Clear()
+            For Each dr As DataRow In ds.Tables(0).Rows
+                lbData.Items.Add(dr("message"))
+                collMessage.Add(dr("message"))
+                collName.Add(dr("name"))
+                collID.Add(dr("id"))
+            Next
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            If (Not dbConnection Is Nothing) Then
+                dbConnection.Close()
+            End If
+        End Try
+    End Sub
+
+    Private Sub btnQuery_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnQuery.Click
+
+     
+        RefreshData()
+
+
+
+
+    End Sub
+
+    Private Sub btnInsert_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnInsert.Click
+
+        Dim connectionWithCredentials As String = String.Format(CONNECTION_STRING, txtUserName.Text, txtPassword.Text)
+
+        Dim dbConnection As MySqlConnection = Nothing
+        Dim command As String = String.Empty
+        command = "INSERT INTO `vb_demo`.`helloworld` (`message`, `name`) VALUES ('{0}', '{1}');"
+        command = String.Format(command, txtMessage.Text, txtName.Text)
+        Try
+
+            dbConnection = New MySqlConnection(connectionWithCredentials)
+            dbConnection.Open()
+            Dim dbCommand As MySqlCommand = New MySqlCommand(command, dbConnection)
+            dbCommand.ExecuteNonQuery()
+            RefreshData()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            If (Not dbConnection Is Nothing) Then
+                dbConnection.Close()
+            End If
+        End Try
+
+    End Sub
+
+    Private Sub lbData_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lbData.SelectedIndexChanged
+
+        Dim tempIndex As Integer = lbData.SelectedIndex
+        If (tempIndex + 1 > collName.Count) Then
+            MessageBox.Show("outside of index")
+        Else
+            txtName.Text = collName(tempIndex + 1)
+            txtMessage.Text = collMessage(tempIndex + 1)
+            lblID.Text = collID(tempIndex + 1)
+
+        End If
+     
+
+    End Sub
+
+    Private Sub ExecuteSqlCommand(ByVal command As String)
+        Dim connectionWithCredentials As String = String.Format(CONNECTION_STRING, txtUserName.Text, txtPassword.Text)
+        Dim dbConnection As MySqlConnection = Nothing
+        Try
+            dbConnection = New MySqlConnection(connectionWithCredentials)
+            dbConnection.Open()
+            Dim dbCommand As MySqlCommand = New MySqlCommand(command, dbConnection)
+            dbCommand.ExecuteNonQuery()
+            RefreshData()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            If (Not dbConnection Is Nothing) Then
+                dbConnection.Close()
+            End If
+        End Try
+    End Sub
+
+
+    Private Sub btnDelete_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnDelete.Click
+        Dim id As String = collID(lbData.SelectedIndex + 1)
+        Dim command As String = "DELETE FROM helloworld WHERE id={0};"
+        command = String.Format(command, id)
+
+        ExecuteSqlCommand(command)
+    End Sub
+
+    Private Sub btnUpdate_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnUpdate.Click
+      
+
+        Dim id As String = collID(lbData.SelectedIndex + 1)
+        Dim command As String = "UPDATE helloworld SET message = '{0}', name = '{1}' WHERE id= {2};"
+        Dim message As String = txtMessage.Text.Replace(";", String.Empty)
+        Dim name As String = txtName.Text.Replace(";", String.Empty)
+        command = String.Format(command, message, name, id)
+        ExecuteSqlCommand(command)
+
+    End Sub
+End Class
