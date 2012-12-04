94fabc621acc467befab622670f6a9c412af2f8e
Joe Baldwin <contact@humboldttechgroup.com>
2012-12-04 11:29:39 -0600

    UI and Data layer separate

---
+++ 12_04_12/CrUD/FVTC.Messages.Data/EC2MySQL.vb
@@ -0,0 +1,62 @@
+﻿Imports MySql.Data
+Imports MySql.Data.MySqlClient
+Imports System.Data
+
+Public Class EC2MySQL
+
+    'replace the <USERNAME> and <PASSWORD> with those provided out on blackboard
+    Private Const CONNECTION_STRING = "server=23.23.147.156;user id=<USERNAME>;password=<PASSWORD>;database=vb_demo;pooling=false;"
+
+    Private _connectionString As String
+
+    Public Sub New()
+        'initialize stuff
+
+        _connectionString = CONNECTION_STRING
+
+    End Sub
+
+
+    Public Sub New(connectionString As String)
+
+        _connectionString = connectionString
+
+    End Sub
+
+
+    Public Shared Function SelectData(query As String) As DataSet
+        If (CONNECTION_STRING.Contains("<USERNAME>")) Then
+            Throw New Exception("Did you forget to replace the credentials in the connection string?")
+        End If
+        Dim dbConnection As MySqlConnection = Nothing
+        Dim dbAdapter As MySqlDataAdapter
+        Dim ds As DataSet = New DataSet()
+        dbConnection = New MySqlConnection(CONNECTION_STRING)
+        dbConnection.Open()
+        dbAdapter = New MySqlDataAdapter(query, dbConnection)
+        dbAdapter.Fill(ds)
+
+        If (Not dbConnection Is Nothing) Then
+            dbConnection.Close()
+        End If
+
+        Return ds
+    End Function
+
+    Public Shared Sub ExecuteSqlCommand(ByVal command As String)
+        If (CONNECTION_STRING.Contains("<USERNAME>")) Then
+            Throw New Exception("Did you forget to replace the credentials in the connection string?")
+        End If
+        Dim dbConnection As MySqlConnection = Nothing
+        dbConnection = New MySqlConnection(CONNECTION_STRING)
+        dbConnection.Open()
+        Dim dbCommand As MySqlCommand = New MySqlCommand(command, dbConnection)
+        dbCommand.ExecuteNonQuery()
+        If (Not dbConnection Is Nothing) Then
+            dbConnection.Close()
+        End If
+    End Sub
+
+
+
+End Class
---
+++ 12_04_12/CrUD/databaseMySql/Form1.vb
@@ -0,0 +1,65 @@
+﻿Imports FVTC.Messages.Data
+
+Public Class Form1
+
+    Private collMessage As Collection = New Collection()
+    Private collName As Collection = New Collection()
+    Private collID As Collection = New Collection()
+
+    Private Sub RefreshData()
+        Dim ds As DataSet = New DataSet()
+        Dim query As String = "SELECT * FROM helloworld"
+        ds = EC2MySQL.SelectData(query)
+
+        collMessage.Clear()
+        collName.Clear()
+        collID.Clear()
+        For Each dr As DataRow In ds.Tables(0).Rows
+            lbData.Items.Add(dr("message"))
+            collMessage.Add(dr("message"))
+            collName.Add(dr("name"))
+            collID.Add(dr("id"))
+        Next
+    End Sub
+
+    Private Sub btnQuery_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnQuery.Click
+        RefreshData()
+    End Sub
+
+    Private Sub btnInsert_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnInsert.Click
+        Dim command As String = String.Empty
+        command = "INSERT INTO `vb_demo`.`helloworld` (`message`, `name`) VALUES ('{0}', '{1}');"
+        command = String.Format(command, txtMessage.Text, txtName.Text)
+        EC2MySQL.ExecuteSqlCommand(command)
+        RefreshData()
+    End Sub
+
+    Private Sub lbData_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lbData.SelectedIndexChanged
+        Dim tempIndex As Integer = lbData.SelectedIndex
+        If (tempIndex + 1 > collName.Count) Then
+            MessageBox.Show("outside of index")
+        Else
+            txtName.Text = collName(tempIndex + 1)
+            txtMessage.Text = collMessage(tempIndex + 1)
+            lblID.Text = collID(tempIndex + 1)
+        End If
+    End Sub
+
+    Private Sub btnDelete_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnDelete.Click
+        Dim id As String = collID(lbData.SelectedIndex + 1)
+        Dim command As String = "DELETE FROM helloworld WHERE id={0};"
+        command = String.Format(command, id)
+        EC2MySQL.ExecuteSqlCommand(command)
+        RefreshData()
+    End Sub
+
+    Private Sub btnUpdate_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnUpdate.Click
+        Dim id As String = collID(lbData.SelectedIndex + 1)
+        Dim command As String = "UPDATE helloworld SET message = '{0}', name = '{1}' WHERE id= {2};"
+        Dim message As String = txtMessage.Text.Replace(";", String.Empty)
+        Dim name As String = txtName.Text.Replace(";", String.Empty)
+        command = String.Format(command, message, name, id)
+        EC2MySQL.ExecuteSqlCommand(command)
+        RefreshData()
+    End Sub
+End Class
