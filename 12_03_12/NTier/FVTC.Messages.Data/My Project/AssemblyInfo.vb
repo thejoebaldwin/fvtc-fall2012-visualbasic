5d2e42341c091b349cc55db4a52f036725d43337
Joe Baldwin <contact@humboldttechgroup.com>
2012-12-03 21:14:31 -0600

    Synch

---
+++ 12_03_12/NTier/FVTC.Messages.Data/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("FVTC.Messages.Data")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("FVTC.Messages.Data")> 
+<Assembly: AssemblyCopyright("Copyright ©  2012")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("939696b2-c29c-467e-be4a-89b400c0d5a7")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ 12_03_12/NTier/FVTC.Messages.Data/MySQL.vb
@@ -0,0 +1,27 @@
+﻿Imports MySql.Data
+Imports MySql.Data.MySqlClient
+Imports System.Data
+
+Public Class MySQL
+
+
+    Private Const CONNECTION_STRING = "server=23.23.147.156; user id=vb_user; password=test!234; database=vb_demo; pooling=false;"
+
+    Public Shared Sub ExecuteSqlCommand(ByVal command As String)
+
+        Dim dbConnection As MySqlConnection = Nothing
+
+        dbConnection = New MySqlConnection(CONNECTION_STRING)
+        dbConnection.Open()
+        Dim dbCommand As MySqlCommand = New MySqlCommand(command, dbConnection)
+        dbCommand.ExecuteNonQuery()
+
+
+        If (Not dbConnection Is Nothing) Then
+            dbConnection.Close()
+        End If
+
+    End Sub
+
+
+End Class
