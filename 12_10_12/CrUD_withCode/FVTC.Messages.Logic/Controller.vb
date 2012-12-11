254e6c13a7c8dd99c6cb68ae744dba9085f0cef0
Joe Baldwin <contact@humboldttechgroup.com>
2012-12-10 20:59:03 -0600

    logic layer, review doc

---
+++ 12_10_12/CrUD_withCode/FVTC.Messages.Logic/Controller.vb
@@ -0,0 +1,44 @@
+﻿
+Imports FVTC.Messages.Data
+
+Public Class Controller
+
+
+    Public Shared Function getAllMessages() As DataSet
+        Dim ds As DataSet = New DataSet()
+        Dim query As String = "SELECT * FROM helloworld"
+        ds = EC2MySQL.SelectData(query)
+        Return ds
+    End Function
+
+    Public Shared Sub addNewMessage(name As String, message As String)
+        Dim command As String = String.Empty
+        command = "INSERT INTO `vb_demo`.`helloworld` (`message`, `name`) VALUES ('{0}', '{1}');"
+        command = String.Format(command, message, name)
+        EC2MySQL.ExecuteSqlCommand(command)
+    End Sub
+
+    Private Shared Function cleanInput(input As String) As String
+        input = input.Replace("#", String.Empty)
+        input = input.Replace("$", String.Empty)
+        input = input.Replace("@@@", String.Empty)
+        Return input.Replace(";", String.Empty)
+    End Function
+
+
+    Public Shared Sub updateMessage(name As String, message As String, id As String)
+        Dim command As String = "UPDATE helloworld SET message = '{0}', name = '{1}' WHERE id= {2};"
+        message = cleanInput(message)
+        name = cleanInput(name)
+        command = String.Format(command, message, name, id)
+        EC2MySQL.ExecuteSqlCommand(command)
+    End Sub
+
+    Public Shared Sub deleteMessage(id As String)
+        Dim command As String = "DELETE FROM helloworld WHERE id={0};"
+        command = String.Format(command, id)
+        EC2MySQL.ExecuteSqlCommand(command)
+    End Sub
+
+
+End Class
---
+++ 12_10_12/CrUD_withCode/databaseMySql/Form1.vb
@@ -0,0 +1,56 @@
+﻿Imports FVTC.Messages.Data
+Imports FVTC.Messages.Logic
+
+Public Class Form1
+
+    Private collMessage As Collection = New Collection()
+    Private collName As Collection = New Collection()
+    Private collID As Collection = New Collection()
+
+    Private Sub RefreshData()
+        Dim ds As DataSet = Controller.getAllMessages()
+
+        collMessage.Clear()
+        collName.Clear()
+        collID.Clear()
+        lbData.Items.Clear()
+        For Each dr As DataRow In ds.Tables(0).Rows
+            lbData.Items.Add(dr("message"))
+            collMessage.Add(dr("message"))
+            collName.Add(dr("name"))
+            collID.Add(dr("id"))
+        Next
+    End Sub
+
+    Private Sub btnQuery_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnQuery.Click
+        RefreshData()
+    End Sub
+
+    Private Sub btnInsert_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnInsert.Click
+        Controller.addNewMessage(txtName.Text, txtMessage.Text)
+        RefreshData()
+    End Sub
+
+    Private Sub lbData_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lbData.SelectedIndexChanged
+        Dim tempIndex As Integer = lbData.SelectedIndex
+        If (tempIndex + 1 > collName.Count) Then
+            MessageBox.Show("outside of index")
+        Else
+            txtName.Text = collName(tempIndex + 1)
+            txtMessage.Text = collMessage(tempIndex + 1)
+            lblID.Text = collID(tempIndex + 1)
+        End If
+    End Sub
+
+    Private Sub btnDelete_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnDelete.Click
+        Dim id As String = collID(lbData.SelectedIndex + 1)
+        Controller.deleteMessage(id)
+        RefreshData()
+    End Sub
+
+    Private Sub btnUpdate_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnUpdate.Click
+        Dim id As String = collID(lbData.SelectedIndex + 1)
+        Controller.updateMessage(txtName.Text, txtMessage.Text, id)
+        RefreshData()
+    End Sub
+End Class
